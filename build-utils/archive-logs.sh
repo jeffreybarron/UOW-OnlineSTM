e49416081be44a1435e38fa9a0278a8236bcdbaa
Jeffrey Barron <jeff@jeff@jeffreybarron.com>
2019-07-16 11:08:39 +1000

    2.2.8.2-master

--- build-utils/archive-logs.sh
+++ build-utils/archive-logs.sh
@@ -5,7 +5,7 @@ mkdir /srv/www/log-archive/logs/
 mkdir /srv/www/log-archive/ostm/
 
 cp /srv/www/routes/logs/ /srv/www/log-archive/logs/ -r
-cp /srv/www/routes/ostm/logs/ /srv/www/llog-archive/ostm/ -r
+cp /srv/www/routes/ostm/logs/ /srv/www/log-archive/ostm/ -r
 cp /srv/www/routes/ostm/manage/logs/ /srv/www/log-archive/ostm/ -r
 cp /root/.pm2/logs/ /srv/www/log-archive/pm2/ -r
 cp /var/log/nginx/ /srv/www/log-archive/nginx/ -r
